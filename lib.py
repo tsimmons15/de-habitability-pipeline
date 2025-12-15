33ac6e243c5c9c16d89e33f6be346afa5d463f92
Timothy <derriese15@gmail.com>
2025-12-15 15:15:14 +0000

    Working my way through bronze.

--- lib.py
+++ lib.py
@@ -1,33 +1,47 @@
-import requests, json, psycopg2, csv, io, os, sys, pandas as pd
+import requests, json, psycopg2, csv, io, os, sys, pandas as pd, re
+from datetime import datetime
+
 
 # API Endpoints
 api_endpoints = {
     "usgs": "https://earthquake.usgs.gov/fdsnws/event/1/query",
     "weather": "https://api.openweathermap.org/data/3.0/onecall/timemachine",
+    "geocode": "http://api.openweathermap.org/geo/1.0/direct",
     "census":"https://api.census.gov/data/2019/pep/charagegroups"
 }
 
 
 #Pull in the USGS data
-def usgs_import(table_name, csv_file):
+def usgs_import(table_name, csv_file, pull_start, pull_end):
     payload = {
         'format':'geojson',
-        'starttime':'2025-12-11T00:00:00.0000',
-        'endtime':'2025-12-11T18:00:00.0000',
+        'starttime':pull_start,
+        'endtime':pull_end,
         'minmagnitude':'5'
     }
     r = requests.get(api_endpoints["usgs"], params=payload)
-    print(r.text)
 
     json_obj = json.loads(r.text)
-    df = pd.json_normalize(json_obj)
+    json_properties = json_obj["features"][0]["properties"]
+    json_geometry = json_obj["features"][0]["geometry"]["coordinates"]
+    json_properties["latitude"] = json_geometry[1]
+    json_properties["longitude"] = json_geometry[0]
+    del json_properties["url"]
+    del json_properties["detail"]
+    json_properties["time"] = datetime.fromtimestamp(json_properties["time"]/1000)
+    json_properties["updated"] = datetime.fromtimestamp(json_properties["updated"]/1000)
+    df = pd.json_normalize(json_properties)
 
     df.to_csv(csv_file, index=False, encoding='utf-8')
 
     uploadCSV(table_name, csv_file)
 
+    # Get rid of stale data
+    if os.path.exists(csv_file):
+        os.remove(csv_file)
 
-def weather_import(table_name, csv_file):
+
+def weather_import(table_name, csv_file, search_time, search_lat, search_lon):
     payload = {
         "lat":"38.83",
         "lon":"-122.83",
@@ -35,7 +49,7 @@ def weather_import(table_name, csv_file):
         "appid":api_config["weather_key"]
     }
     r = requests.get(api_endpoints["weather"], params=payload)
-    print(r.text)
+    #print(r.text)
 
     json_obj = json.loads(r.text)
     df = pd.json_normalize(json_obj)
@@ -44,7 +58,13 @@ def weather_import(table_name, csv_file):
 
     uploadCSV(table_name, csv_file)
 
-def census_import(table_name, csv_file):
+    # Get rid of stale data
+    if os.path.exists(csv_file):
+        os.remove(csv_file)
+
+def census_import(table_name, csv_file, geocode_table, geocode_file):
+    pattern = re.compile("(.+) County")
+
     payload = {
         "get":"NAME,POP",
         "HISP":"0",
@@ -52,15 +72,51 @@ def census_import(table_name, csv_file):
         "in":"state:*"
     }
     r = requests.get(api_endpoints["census"], params=payload)
-    print(r.text)
 
+    geocode_result = []
     json_obj = json.loads(r.text)
-    df = pd.json_normalize(json_obj)
+    for c in json_obj[1:]:
+        (area, state) = c[0].split(",")
+        match = pattern.match(area)
+        if match is not None or pattern.groups > 1:
+            area_name = match.group(1)
+        else:
+            area_name = area
 
+        
+        geocode_payload = {
+            "q":f"{area_name}",
+            "limit":"5",
+            "appid":api_config["weather_key"]
+        }
+
+        r = requests.get(api_endpoints["geocode"], params=geocode_payload)
+        if r.status_code == requests.codes.ok and len(r.text) > 2:
+            geocode_json = json.loads(r.text)
+            for g in geocode_json:
+                if g["country"] == "US":
+                    geocode_result.append(g)
+
+        else:
+            print(f"Unable to get details for {area_name}({area}) in {state}")
+
+
+    df = pd.json_normalize(json_obj)
     df.to_csv(csv_file, index=False, encoding='utf-8')
 
-    uploadCSV(table_name, csv_file)
+    geocode_df = pd.json_normalize(geocode_result)
+    df.to_csv(geocode_file, index=False, encoding='utf-8')
 
+    uploadCSV(table_name, csv_file)
+    uploadCSV(geocode_table, geocode_file)
+
+    # Get rid of stale data
+    if os.path.exists(csv_file):
+        os.remove(csv_file)
+    if os.path.exists(geocode_file):
+        os.remove(geocode_file)
+
+    return geocode_result
 
 def uploadCSV(table_name, csv_file, sep=','):
     conn = None
@@ -70,7 +126,7 @@ def uploadCSV(table_name, csv_file, sep=','):
         cursor = conn.cursor()
 
         # Open the CSV file and use copy_from
-        with open(csv_file_path, 'r') as f:
+        with open(csv_file, 'r') as f:
             # Skip the header row if present in the CSV
             f.readline()
             cursor.copy_from(f, table_name, sep=sep)
--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 #User-defined libraries
 from lib import usgs_import, weather_import, census_import
+from datetime import datetime, timedelta
+
+#CSV Directory path
+csv_dir = r"/home/ec2-user/ukus18nov/tsimmons/project/raw_data"
 
 #API Calls
-usgs_import("usgs_raw", "usgs_raw.csv")
+usgs_end = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+usgs_start = usgs_end - timedelta(days=1)
 
-weather_import("weather_raw", "weather_raw.csv")
+usgs_import("usgs_raw", f"{csv_dir}/usgs_raw.csv", usgs_start, usgs_end)
 
-census_import("census_raw", "census_raw.csv")
+geocode_json = census_import("census_raw", f"{csv_dir}/census_raw.csv", "geocode_raw", f"{csv_dir}/geocode_raw.csv")
 
-
-
+print(geocode_json)
+for g in geocode_json:
+    dt = usgs_start
+    while dt <= usgs_end:
+        #print(f"Running weather import for {dt}, {g['lat']}, {g['lon']}")
+        weather_import("weather_raw", f"{csv_dir}/weather_raw.csv", dt.timestamp(), g["lat"], g["lon"])
+        dt = dt + timedelta(hours=1)
