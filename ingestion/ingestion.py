c0785dd8a8a343c583ba878f1320a92bfca4bf1e
Timothy <derriese15@gmail.com>
2025-12-20 13:41:46 +0000

    Fixed another issue caused by the switch to a daily summary. Changed a reference to dt to usgs_end, the time truncated datestamp passed to the weather API

--- ingestion/ingestion.py
+++ ingestion/ingestion.py
@@ -81,7 +81,7 @@ def start():
     for g in geocode_json:
         logger.info(f"Running weather import for {dt.strftime('%Y-%m-%d')}, {g['lat']}, {g['lon']}")
         if reprocess is not None and reprocess == 'False':
-            weather_import(f"{csv_dir}/{weather_filename}", dt.strftime("%Y-%m-%d"), g["lat"], g["lon"])
+            weather_import(f"{csv_dir}/{weather_filename}", usgs_end.strftime("%Y-%m-%d"), g["lat"], g["lon"])
 
         cols = "lat, lon, tz, date, units, cloud_cover, humidity, precipitation, temperature_min, temperature_max, pressure, wind"
         uploadCSV("weather_insert", f"{csv_dir}/{weather_filename}", cols=cols)
